6088eedfca7f7946dcfb4130f7d79f41785a22e7
Jeff <kanardito14@free.fr>
2016-04-11 20:12:23 +0200

    Mise à jour de makelinks.sh pour init.el

--- makelinks.sh
+++ makelinks.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-files="bashrc conkyrc emacs gitconfig zshrc dircolors \
-       environ profile tmux.conf"
+homefiles="bashrc conkyrc emacs gitconfig zshrc dircolors \
+           environ profile tmux.conf"
+emacsfiles="init.el"
 confdir=Documents/Config
 h=$(hostname)
 
-for f in $files ; do
+for f in $homefiles ; do
 
     if [ -f $f.$h ] ; then
 	echo "Link $f.$h to ~/.$f"
@@ -14,5 +15,12 @@ for f in $files ; do
 	echo "Link $f to ~/.$f"
 	ln -sf $confdir/$f ~/.$f
     fi
+
+done
+
+for f in $emacsfiles ; do
+
+    echo "Link $f to ~/.emacs.d/$f"
+    ln -sf $confdir/$f ~/.emacs.d/$f
 
 done
